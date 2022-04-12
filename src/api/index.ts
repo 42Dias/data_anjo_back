2419b618704233fbfc5b35b13a2813e39cdc0de5
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-12 09:26:26 -0300

    try with cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -15,6 +15,16 @@ const app = express();
 // Enables CORS
 app.use(cors({ origin: true }));
 
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+  app.use(cors());
+  next();
+});
+
+
 // Initializes and adds the database middleware.
 app.use(databaseMiddleware);
 
