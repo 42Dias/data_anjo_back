460c2c34b21adfaced5d15420ad81664b4d24b7e
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-26 15:05:27 -0300

    added more filters

--- src/database/repositories/cerimoniaRepository.ts
+++ src/database/repositories/cerimoniaRepository.ts
@@ -352,6 +352,7 @@ class CerimoniaRepository {
       }
 
       if (filter.idFuneraria) {
+
         whereAnd.push({
           ['idFunerariaId']: SequelizeFilterUtils.uuid(
             filter.idFuneraria,
